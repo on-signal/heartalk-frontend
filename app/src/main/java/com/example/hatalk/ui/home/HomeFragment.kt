e5c5ae4100bd51f70213a30fdadf1653bdfaca6d
Jaeyun <v44ads@naver.com>
2021-11-15 10:03:01 +0900

    git init

--- app/src/main/java/com/example/hatalk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hatalk/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -31,9 +32,9 @@ class HomeFragment : Fragment() {
         _binding=FragmentHomeBinding.inflate(inflater, container, false)
         val root: View=binding.root
 
-        val textView: TextView=binding.textHome
+        val matchingButton: Button=binding.matchingButton
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text=it
+            matchingButton.text=it
         })
         return root
     }
--- app/src/main/java/com/example/hatalk/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/hatalk/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text=MutableLiveData<String>().apply {
-        value="This is home Fragment"
+        value="매칭하기"
     }
     val text: LiveData<String> = _text
 }
