94928bd7243c962a83d188ffc95d901cb6a50024
Jaeyun <v44ads@naver.com>
2021-11-15 18:11:50 +0900

    signal room upgrade

---
+++ app/src/main/java/com/example/hatalk/signalRoom/SignalRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hatalk.signalRoom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hatalk.R
+
+class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
