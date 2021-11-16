00dc072afac69ce283ac208e8fb549cb9b1ca4ff
parkchoongho <pchyo92@gmail.com>
2021-11-16 23:11:07 +0900

    로그인시 코루틴 버그 수정

--- app/src/main/java/com/example/hatalk/model/UserAuthModel.kt
+++ app/src/main/java/com/example/hatalk/model/UserAuthModel.kt
@@ -1,59 +1,74 @@
 package com.example.hatalk.model
 
+import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.hatalk.SignUpActivity
 import com.example.hatalk.network.LoginRequest
 import com.example.hatalk.network.UserApi
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class UserAuthModel: ViewModel() {
-    private val _email = MutableLiveData<String>()
-    val email: LiveData<String> get() = _email
+    private var _email:String? = null
+    val email: String? get() = _email
 
-    private val _photoUrl = MutableLiveData<String>()
-    val photoUrl: LiveData<String> get() = _photoUrl
+    private var _name: String? = null
+    val name: String? get() = _name
 
-    private val _accessToken = MutableLiveData<String>()
-    val accessToken: LiveData<String> get() = _accessToken
+    private var _photoUrl:String? = null
+    val photoUrl: String? get() = _photoUrl
 
-    private val _refreshToken = MutableLiveData<String>()
-    val refreshToken: LiveData<String> get() = _refreshToken
+    private var _accessToken: String? = null
+    val accessToken: String? get() = _accessToken
 
-    private val _loginSuccess = MutableLiveData<Boolean>()
-    val loginSuccess: LiveData<Boolean> get() = _loginSuccess
+    private var _refreshToken: String? = null
+    val refreshToken: String? get() = _refreshToken
+
+    private var _loginSuccess: Boolean? = null
+
+    fun getLoginSucess(): Boolean? {
+        return _loginSuccess
+    }
 
     fun setEmail(email: String) {
-        if (_email.value != null) {
+        if (_email != null) {
             return
         } else {
-            _email.value = email
+            _email = email
         }
     }
 
     fun setProfileUrl(profileUrl: String) {
-        if(_photoUrl.value != null) {
+        if(_photoUrl != null) {
             return
         } else {
-            _photoUrl.value = profileUrl
+            _photoUrl = profileUrl
         }
     }
 
-     fun login(body: LoginRequest) {
-        viewModelScope.launch {
+    fun setNickname(nickname: String) {
+        if(_name != null) {
+            return
+        } else {
+            _name = nickname
+        }
+    }
+
+     fun login(body: LoginRequest): Job {
+        return viewModelScope.launch(Dispatchers.IO)  {
             try {
                 val response = UserApi.retrofitService.login(body)
-                Log.d("TEST: ", response.toString())
-                _loginSuccess.value = response.code() != 404
-
-//                _accessToken.value = user.body()?.accessToken
-//                _refreshToken.value = user.body()?.refreshToken
+                _loginSuccess = response.code().toString() != "401"
+                _accessToken = response.body()?.accessToken
             } catch (e: Exception) {
-                Log.d("TEST: ", e.toString())
-                _loginSuccess.value = false
+                _loginSuccess = false
             }
         }
     }
