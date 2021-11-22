cf525361cf3514deb21220b1f069b2661d7e585c
parkchoongho <pchyo92@gmail.com>
2021-11-22 20:40:24 +0900

    시그널룸 채팅

--- app/src/main/java/com/example/hatalk/signalRoom/sigRoom/SignalRoomActivity.kt
+++ app/src/main/java/com/example/hatalk/signalRoom/sigRoom/SignalRoomActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.RelativeLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.cometchat.pro.constants.CometChatConstants
 import com.cometchat.pro.core.AppSettings
@@ -20,28 +18,45 @@ import com.cometchat.pro.models.User
 import com.example.hatalk.R
 import com.example.hatalk.network.MatchingConfirmResponse
 import com.example.hatalk.signalRoom.PRIVATE.IDs
+import com.google.gson.Gson
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers.IO
-import java.net.Socket
+import org.json.JSONObject
 import java.net.URISyntaxException
 import java.util.*
+
+class SocketApplication {
+    companion object {
+        private lateinit var socket: Socket
+        fun get(): Socket {
+            try {
+                // [uri]부분은 "http://X.X.X.X:3000" 꼴로 넣어주는 게 좋다.
+                socket = IO.socket("http://3.18.104.98:8000/chat")
+            } catch (e: URISyntaxException) {
+                e.printStackTrace()
+            }
+            return socket
+        }
+    }
+}
 
 /** [Permission] 처리해줘야 함!!!--------------------------------------------- */
 class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
     private val TAG = "HEART"
-//    lateinit var mSocket: Socket
-//    private val onConnect = Emitter.Listener {
-//        mSocket.emit("emitReceive", "OK")
-//    }
+    lateinit var mSocket: Socket
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val appID:String= IDs.APP_ID // Replace with your App ID
-        val region:String=IDs.REGION // Replace with your App Region ("eu" or "us")
+        val appID: String = IDs.APP_ID // Replace with your App ID
+        val region: String = IDs.REGION // Replace with your App Region ("eu" or "us")
 
-        val appSettings = AppSettings.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build()
-        CometChat.init(this,appID,appSettings, object : CometChat.CallbackListener<String>() {
+        val appSettings =
+            AppSettings.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region)
+                .build()
+        CometChat.init(this, appID, appSettings, object : CometChat.CallbackListener<String>() {
             override fun onSuccess(p0: String?) {
                 Log.d(TAG, "Initialization completed successfully")
             }
@@ -54,8 +69,9 @@ class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
 
         val intent: Intent = getIntent()
         val matchingData = intent.getParcelableExtra<MatchingConfirmResponse>("matchingData")
+
         /** [Cometchat_init] ------------------------------------------------ */
-        val userID:String = CometChat.getLoggedInUser().uid.toString()
+        val userID: String = CometChat.getLoggedInUser().uid.toString()
         findViewById<Button>(R.id.button_chat_send).setOnClickListener {
             Log.d(TAG, userID)
         }
@@ -76,21 +92,43 @@ class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
         /**
          * [Socket IO Chat Start]
          */
+        val chat_button = findViewById<Button>(R.id.button_chat_send)
+        val chat_text = findViewById<EditText>(R.id.edit_chat_message)
+        chat_button.setOnClickListener {
+            mSocket = SocketApplication.get()
+            mSocket.connect()
+            val message = TempMessage("channel1", userID, chat_text.text.toString())
+            val gson = Gson()
+            val obj = JSONObject(gson.toJson(message))
+            val onConnect = Emitter.Listener { args ->
+                val res = JSONObject(args[0].toString())
+                val userChat = findViewById<TextView>(R.id.chat_text_man_1)
+                userChat.text = res.getString("text")
+            }
+            mSocket.on("channel1", onConnect)
+            mSocket.emit("msgToServer", obj)
+        }
+
 
 //        try {
-//            mSocket = IO.socket("http://localhost/8000")
+//            mSocket = IO.socket("http://10.0.2.2:8000")
+//            Log.d("Socket: ", mSocket.toString())
 //            mSocket.connect()
+//            val message = TempMessage("parkchoongho", "Hello~")
+//            mSocket.on("channel1") { msg ->
+//                Log.d("Message: ", msg.toString())
+//            }
+//            mSocket.emit("msgToServer", message)
+//
 //            Log.d("Connected", "OK")
 //        } catch (e: URISyntaxException) {
 //            Log.d("ERR", e.toString())
 //        }
-//        mSocket.on(Socket.EVENT_CONNECT, onConnect)
 
         /**
          * [Socket IO Chat End]
          */
     }
-
 
 
     private fun initiateCall(guid: String) {
@@ -100,7 +138,7 @@ class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
 
         val call = Call(receiverID, receiverType, callType)
 
-        CometChat.initiateCall(call,object :CometChat.CallbackListener<Call>(){
+        CometChat.initiateCall(call, object : CometChat.CallbackListener<Call>() {
             override fun onSuccess(p0: Call?) {
                 Log.d(TAG, "Call initiated successfully: " + p0?.toString())
                 startCall(p0)
@@ -118,49 +156,50 @@ class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
         var callView: RelativeLayout? = findViewById(R.id.sig_room_ui)
         val activity: Activity? = this
 
-        val callSettings = CallSettings.CallSettingsBuilder(activity,callView)
+        val callSettings = CallSettings.CallSettingsBuilder(activity, callView)
             .setSessionId(sessionID)
             .setAudioOnlyCall(true)
             .build();
 
-        CometChat.startCall(callSettings, object:  CometChat.OngoingCallListener {
+        CometChat.startCall(callSettings, object : CometChat.OngoingCallListener {
 
             override fun onUserJoined(user: User?) {
-                Log.d(TAG, "onUserJoined: Name "+user!!.getName());
+                Log.d(TAG, "onUserJoined: Name " + user!!.getName());
             }
 
             override fun onUserLeft(user: User?) {
-                Log.d(TAG, "onUserLeft: "+user!!.getName());
+                Log.d(TAG, "onUserLeft: " + user!!.getName());
             }
 
             override fun onError(e: CometChatException?) {
-                Log.d(TAG, "onError: "+ e!!.message);
+                Log.d(TAG, "onError: " + e!!.message);
             }
 
             override fun onCallEnded(call: Call?) {
-                Log.d(TAG, "onCallEnded: "+ call.toString());
+                Log.d(TAG, "onCallEnded: " + call.toString());
             }
 
             override fun onUserListUpdated(p0: MutableList<User>?) {
-                Log.d(TAG, "onUserListUpdated: "+ call.toString());
+                Log.d(TAG, "onUserListUpdated: " + call.toString());
             }
 
             override fun onAudioModesUpdated(p0: MutableList<AudioMode>?) {
-                Log.d(TAG, "onAudioModesUpdated: "+ call.toString());
+                Log.d(TAG, "onAudioModesUpdated: " + call.toString());
             }
         });
         findViewById<RelativeLayout>(R.id.sig_room_ui).visibility = View.GONE
-        Toast.makeText(this,"연결 됐습니다.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "연결 됐습니다.", Toast.LENGTH_SHORT).show()
     }
 
     private fun addCallListener() {
-        val listenerID:String= "chatlist_call_" + Date().time.toString();
+        val listenerID: String = "chatlist_call_" + Date().time.toString();
 
-        CometChat.addCallListener(listenerID,object : CometChat.CallListener(){
+        CometChat.addCallListener(listenerID, object : CometChat.CallListener() {
             override fun onOutgoingCallAccepted(p0: Call?) {
                 Log.d(TAG, "Outgoing call accepted: " + p0?.toString())
                 acceptCall(p0!!)
             }
+
             override fun onIncomingCallReceived(p0: Call?) {
                 Log.d(TAG, "Incoming call: " + p0?.toString())
                 acceptCall(p0!!)
@@ -176,10 +215,10 @@ class SignalRoomActivity : AppCompatActivity(R.layout.activity_signal_room) {
         })
     }
 
-    private fun acceptCall(call:Call) {
-        val sessionID:String = call.sessionId
+    private fun acceptCall(call: Call) {
+        val sessionID: String = call.sessionId
 
-        CometChat.acceptCall(sessionID,object :CometChat.CallbackListener<Call>(){
+        CometChat.acceptCall(sessionID, object : CometChat.CallbackListener<Call>() {
             override fun onSuccess(p0: Call?) {
                 Log.d("", "Call accepted successfully: " + p0?.toString())
                 startCall(p0)
---
+++ app/src/main/java/com/example/hatalk/signalRoom/sigRoom/TempMessage.kt
@@ -0,0 +1,7 @@
+package com.example.hatalk.signalRoom.sigRoom
+
+data class TempMessage(
+    val channelname: String,
+    val name: String,
+    val text: String
+)
