0c7b3e30c6d846ad9934caa5d7202a6afa0c5fb1
Jaeyun <v44ads@naver.com>
2021-12-07 19:23:30 +0900

    메인 홈 UI

--- app/src/main/java/com/heartsignal/hatalk/main/MainHomeFragment.kt
+++ app/src/main/java/com/heartsignal/hatalk/main/MainHomeFragment.kt
@@ -20,6 +20,8 @@ import android.widget.Button
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.cometchat.pro.constants.CometChatConstants
 import com.cometchat.pro.core.CometChat
@@ -105,7 +107,7 @@ class MainHomeFragment : Fragment() {
 
         Glide.with(this)
             .load(GlobalApplication.userInfo.photoUrl)
-            .apply(RequestOptions().circleCrop())
+            .transform(CenterCrop(), RoundedCorners(100))
             .into(binding!!.homeProfileImage)
 
 
