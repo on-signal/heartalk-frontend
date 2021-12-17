c56381b1d5a8c6ebc7fcbad65fc74946c385f66a
Jaeyun <v44ads@naver.com>
2021-12-17 13:54:27 +0900

    signup modified

--- app/src/main/java/com/heartsignal/hatalk/SignUpFragment.kt
+++ app/src/main/java/com/heartsignal/hatalk/SignUpFragment.kt
@@ -1,17 +1,18 @@
 package com.heartsignal.hatalk
 
 import android.os.Bundle
-import android.util.Log
+import android.telephony.PhoneNumberFormattingTextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.heartsignal.hatalk.R
 import com.heartsignal.hatalk.databinding.FragmentSignUpBinding
 import com.heartsignal.hatalk.model.UserJoinModel
+import java.util.regex.Pattern
 
 
 class SignUpFragment : Fragment() {
@@ -45,12 +46,14 @@ class SignUpFragment : Fragment() {
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     TODO("Not yet implemented")
                 }
-
             }
             signup.setOnClickListener {
                 onSignUp()
             }
         }
+
+        val phoneNumberInput = binding?.textInputEditPhoneNumber
+        phoneNumberInput?.addTextChangedListener(PhoneNumberFormattingTextWatcher())
     }
 
     override fun onDestroyView() {
@@ -59,16 +62,26 @@ class SignUpFragment : Fragment() {
     }
 
      fun onSignUp(){
-        val name = binding?.textInputEditName?.text.toString()
-        val socialNumber = binding?.textInputEditSocialNumber?.text.toString()
-        val phoneNumber = binding?.textInputEditPhoneNumber?.text.toString()
-        val nickname = binding?.textInputEditNickname?.text.toString()
+         val name = binding?.textInputEditName?.text.toString()
+         val socialNumber = binding?.textInputEditSocialNumber?.text.toString()
+         val phoneNumber = binding?.textInputEditPhoneNumber?.text.toString()
+         val nickname = binding?.textInputEditNickname?.text.toString()
 
-        sharedViewModel.setName(name)
-        sharedViewModel.setSocialNumber(socialNumber)
-        sharedViewModel.setPhoneNumber(phoneNumber)
-        sharedViewModel.setNickname(nickname)
-         goToNextScreen()
+         if (checkSocialNumber(socialNumber)) {
+             sharedViewModel.setName(name)
+             sharedViewModel.setSocialNumber(socialNumber)
+             sharedViewModel.setPhoneNumber(phoneNumber)
+             sharedViewModel.setNickname(nickname)
+             goToNextScreen()
+         }
+    }
+
+    private fun checkSocialNumber(sNum: String): Boolean {
+        if (!Pattern.matches("\\d{6}[1-4]", sNum)) {
+            Toast.makeText(context, "주민등록번호 형식을 지켜주세요", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 
     private fun goToNextScreen() {
